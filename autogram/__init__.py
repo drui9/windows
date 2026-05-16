073040bcefaed6d6e6bf34da51a457faa5fac2b7
incognieh <drui9@duck.com>
2026-05-16 10:14:52 +0300

    deprecated autogram support

--- autogram/__init__.py
+++
@@ -1,8 +0,0 @@
-from autogram.actions import ChatAction
-from autogram.types import UpdateType, EventType
-
-
-from autogram.config import Start
-from autogram.autogram import Autogram
-
-__all__ = ["Start", "Autogram", "ChatAction", "UpdateType", "EventType"]
